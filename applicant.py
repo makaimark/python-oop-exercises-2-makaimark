66dacb1487b3e2f7f8a2103a416880631a8663c5
makaimark <makaimark@gmail.com>
2016-07-02 12:56:20 +0200

    1.0

--- applicant.py
+++ applicant.py
@@ -19,7 +19,13 @@ class Applicant():
     # }, ...]
     @classmethod
     def _4_specific_applicant_by_first_name(cls):
-        pass
+        from db import Database
+        applicants = Database.get_applicants()
+        temp_dict = {}
+        for i in applicants:
+            if i.first_name == "Carol":
+                temp_dict["full_name"] = str(i.first_name + " " + i.last_name)
+        return [temp_dict]
 
     # Return the full name, as a full_name property of all the applicants, whose email ends with '@adipiscingenimmi.edu'
     # returns: list of dictionaries
@@ -28,7 +34,16 @@ class Applicant():
     # }, ...]
     @classmethod
     def _5_specific_applicant_by_email_domain(cls):
-        pass
+        from db import Database
+        applicants = Database.get_applicants()
+        return_list = []
+        temp_dict = {}
+        for i in applicants:
+            if '@adipiscingenimmi.edu' in i.email:
+                temp_dict["full_name"] = str(i.first_name + " " + i.last_name)
+                return_list.append(temp_dict)
+                temp_dict = {}
+        return return_list
 
     # Insert a the Applicant into Database.applicants_data,
     # and return a filtered list, where we only add the data of Markus.
@@ -50,7 +65,22 @@ class Applicant():
     # }]
     @classmethod
     def _6_inserting_a_new_applicant(cls):
-        pass
+        from db import Database
+        applicants = Database.get_applicants()
+        temp_dict = {}
+        return_list = []
+        new_applicant = [11, "Markus", "Schaffarzyk", "003620/725-2666", "djnovus@groovecoverage.com", 54823]
+        Database.applicants_data.append(new_applicant)  # Nem appendel :(
+        for i in applicants:
+            if i.id == new_applicant[0]:
+                temp_dict["id"] = int(i.id)
+                temp_dict["first_name"] = i.first_name
+                temp_dict["last_name"] = i.last_name
+                temp_dict["phone_number"] = i.phone_number
+                temp_dict["email"] = i.email
+                temp_dict["application_code"] = int(i.application_code)
+                return_list.append(temp_dict)
+        return return_list
 
     # Update an Applicant in the applicants_data, and returns a filtered dictionary list for checking.
     # Story: Jemima Foreman, an applicant called us, that her phone number changed to: 003670/223-7459
@@ -59,8 +89,8 @@ class Applicant():
     # }]
     @classmethod
     def _7_updating_data(cls):
+        from db import Database
         pass
-
     # Delete lines from the applicants_data, based on a filter condition
     # Story: Arsenio, an applicant called us, that he and his friend applied to Codecool.
     # They both want to cancel the process, because they got an investor for the site they run: mauriseu.net
